ec3750039c95b2e524af3ff4989d19f93f0299e3
igor <igor.php.13@gmail.com>
2018-04-27 22:19:34 +0300

    easy admin

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\ChessMatch;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,8 +14,10 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $repository = $this->getDoctrine()->getRepository(ChessMatch::class)->findAll();
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
+            'repository' => $repository,
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
